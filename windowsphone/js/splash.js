c63617bc680d364a5717f214e765efd1da33b7fa
Ben Wang <imbushuo@outlook.com>
2015-11-28 00:42:46 -0500

    Initial Windows Phone / Modern WIndows version.

---
+++ windowsphone/js/splash.js
@@ -0,0 +1,10 @@
+(function (){
+	document.addEventListener("DOMContentLoaded", function(){
+		var splash = document.getElementById("splashscreen");
+		splash.style.cssText = "display: none;";
+		var content= document.getElementById("coreContent");
+		content.style.cssText = "";
+		var appbar = document.getElementById("coreAppAppBar");
+		appbar.style.cssText = "";
+	},false);
+})();
